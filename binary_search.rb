296c705167f77b016f21d9ebdcdfc7d55ef80b55
tddycks <tadzio.smith@gmail.com>
2017-03-27 21:15:10 -0400

    took out unnecessary to_i method

--- binary_search.rb
+++ binary_search.rb
@@ -1,7 +1,6 @@
 def binary_search(array, number)
   #for this to work, array must be a sorted list
   #number is the number you are searching for in the array
-  num = number.to_i
   min = 0
   max = array.length - 1
 
@@ -9,9 +8,9 @@ def binary_search(array, number)
   # if the number is not in the array, eventually min will be greater than max
   #which will break loop
   index_guess = (min + ((max - min) / 2)).floor # the average of min and max
-    if array[index_guess] == num
-      return "#{num} is in this array in index position #{index_guess}."
-    elsif array[index_guess] > num
+    if array[index_guess] == number
+      return "#{number} is in this array in index position #{index_guess}."
+    elsif array[index_guess] > number
       max = index_guess - 1
     else
       min = index_guess + 1
